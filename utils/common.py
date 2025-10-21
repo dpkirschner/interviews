2117b373509cb3dbb92ceeb880b171f4566bceb4
Dan Kirschner <dpkirschner@gmail.com>
2025-10-21 14:22:13 -0700

    test commit for signing

--- utils/common.py
+++ utils/common.py
@@ -7,21 +7,24 @@ from typing import List, Optional
 
 class ListNode:
     """Definition for singly-linked list."""
+
     def __init__(self, val=0, next=None):
+        # definitely a useful comment here
         self.val = val
         self.next = next
-    
+
     def __repr__(self):
         return f"ListNode({self.val})"
 
 
 class TreeNode:
     """Definition for a binary tree node."""
+
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-    
+
     def __repr__(self):
         return f"TreeNode({self.val})"
 
@@ -30,7 +33,7 @@ def list_to_linked_list(arr: List[int]) -> Optional[ListNode]:
     """Convert a list to a linked list."""
     if not arr:
         return None
-    
+
     head = ListNode(arr[0])
     current = head
     for val in arr[1:]:
